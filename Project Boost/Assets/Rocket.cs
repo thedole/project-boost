a2a45bf125744ba4061fe1c27261d23f767af0ac
Dag Ole Kristoffersen <thedole@gmail.com>
2018-04-09 00:57:19 +0200

    Added a script to rocket with test inputs

---
+++ Project Boost/Assets/Rocket.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+    private Rotating rotating;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessInput();
+	}
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("Thrust");
+        }
+
+        if(Input.GetKey(KeyCode.A) && rotating != Rotating.Right)
+        {
+            rotating = Rotating.Left;
+            print("Rotate Left");
+        }
+        else if (Input.GetKeyUp(KeyCode.A) && rotating == Rotating.Left) {
+            rotating = Rotating.None;
+        }
+
+        if (Input.GetKey(KeyCode.D) && rotating != Rotating.Left)
+        {
+            rotating = Rotating.Right;
+            print("Rotate Right");
+        }
+        else if (Input.GetKeyUp(KeyCode.D) && rotating == Rotating.Right)
+        {
+            rotating = Rotating.None;
+        }
+    }
+    enum Rotating { None, Left, Right };
+}
