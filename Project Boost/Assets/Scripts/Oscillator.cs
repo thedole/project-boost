bfcf1842429c0262ef182a520409baf3f31b2475
Dag Ole Kristoffersen <thedole@gmail.com>
2018-04-19 13:14:24 +0200

    made the oscillator, well.. Oscillate

--- Project Boost/Assets/Scripts/Oscillator.cs
+++ Project Boost/Assets/Scripts/Oscillator.cs
@@ -6,15 +6,24 @@ public class Oscillator : MonoBehaviour {
     [Range(0, 1)]
     [SerializeField]
     private float movementFactor;
+    [SerializeField]
+    private float period = 2f;
+
     private Vector3 startingPosition;
+    private float tau;
 
     // Use this for initialization
     void Start () {
+        tau = Mathf.PI * 2;
         startingPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update () {
+        var cycles = Time.time / period;
+        var rawSine = Mathf.Sin(cycles * tau);
+        movementFactor = rawSine / 2 + .5f;
+
         var offset = movementVector * movementFactor;
         transform.position = startingPosition + offset;
 	}
